de3a1427f92a18b4ad2174ea8e4146fb4d038ed4
Kent Collins <kent.r.collins@gmail.com>
2015-09-19 08:17:02 +0100

    Create LuhnTester.java

---
+++ LuhnTester.java
@@ -0,0 +1,19 @@
+/**
+ * Checks the Luhn class for proper operation
+ */ 
+
+public class LuhnTester {
+
+  public static void main(String[] args){
+    // Write code that invokes the isValid method with a known good value, 
+    // ie 1230, and print the result (should be true)
+    
+    // Write code that invokes the isValid method with a known incorrect value, 
+    // ie 1231, and print the result (should be false)
+    
+    // Write code that invokes the generate method with an int, ie 123, and
+    // print the result.  (should be 0, since 1230 forms a valid Luhn) 
+  
+  }
+  
+}
